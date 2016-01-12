23ec0cc6ce3f4faf4f224e60207ec4ae80c06a04
MFathirIrhas <fathir.irhas@outlook.com>
2016-01-12 17:18:31 +0700

    Browse HostImage

--- TugasAkhir1/Form1.Designer.cs
+++ TugasAkhir1/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fileLocationTxt = new System.Windows.Forms.TextBox();
+            this.hostImage = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hostImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.hostImage);
+            this.panel1.Controls.Add(this.fileLocationTxt);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(306, 295);
+            this.panel1.TabIndex = 1;
+            // 
+            // fileLocationTxt
+            // 
+            this.fileLocationTxt.Location = new System.Drawing.Point(85, 5);
+            this.fileLocationTxt.Name = "fileLocationTxt";
+            this.fileLocationTxt.Size = new System.Drawing.Size(216, 20);
+            this.fileLocationTxt.TabIndex = 1;
+            // 
+            // hostImage
+            // 
+            this.hostImage.Location = new System.Drawing.Point(3, 51);
+            this.hostImage.Name = "hostImage";
+            this.hostImage.Size = new System.Drawing.Size(298, 239);
+            this.hostImage.TabIndex = 2;
+            this.hostImage.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Host Image :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(753, 428);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Tugas Akhir 1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hostImage)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox hostImage;
+        private System.Windows.Forms.TextBox fileLocationTxt;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TugasAkhir1/Form1.cs
+++ TugasAkhir1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TugasAkhir1
 {
@@ -15,6 +16,19 @@ namespace TugasAkhir1
         public Form1()
         {
             InitializeComponent();
+            this.hostImage.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select a Picture";
+            ofd.InitialDirectory = @"C:\Users\Fathir Irhas\Pictures";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileLocationTxt.Text = ofd.FileName;
+                hostImage.Image = new Bitmap(ofd.FileName);
+            }
         }
     }
 }
